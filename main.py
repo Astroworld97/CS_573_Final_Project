dd52126d95929bd9cf8dcc96419e20dac8ea8442
Augusto Savaris <augustosavaris@Augustos-MacBook-Pro.local>
2022-03-13 15:24:57 -0500

    add code used for midterm

---
+++ main.py
@@ -0,0 +1,26 @@
+from cProfile import run
+import numpy as np
+import pandas as pd
+from utilities import *
+import tensorflow as tf
+from evaluation import *
+from neural_net import *
+
+ticker=['VALE3', 'PETR4', 'MGLU3', 'ITUB4', 'GOLL4']
+
+lookback_arr=[10, 20, 50]
+lookahead_arr=[1,5, 20]
+test_err_arr=[]
+conditions=[]
+
+
+for lookback in lookback_arr:
+    for lookahead in lookahead_arr:
+        conditions.append((lookback, lookahead))
+        print(conditions[-1])
+        test_err = run_neural_net(ticker, lookback, lookahead)
+        test_err_arr.append(test_err)
+        
+
+print(test_err_arr)
+print(conditions)
--- neural_net.py
+++ neural_net.py
@@ -2,45 +2,68 @@ import numpy as np
 import pandas as pd
 from utilities import *
 import tensorflow as tf
+from evaluation import *
 
-TICKER='PETR4'
-LOOKBACK=10
+TICKER=['VALE3', 'PETR4', 'MGLU3', 'VVAR3', 'ITUB4', 'GOLL4', 'LREN3', 'WEGE3', 'CTSA4', 'EZTC3', 'GGBR4',
+            'MRFG3', 'SULA11', 'IRBR3', 'TAEE11', 'SUZB3']
+LOOKBACK=20
+LOOKAHEAD=2
 LABEL_COL='close'
 
-#load data
-raw_df=get_ticker_data(TICKER)
+def run_neural_net(TICKER, LOOKBACK, LOOKAHEAD, LABEL_COL='close'):
 
-#get features
-raw_df=create_features(raw_df, lookback=LOOKBACK, label_col=LABEL_COL)
+    #load data
+    raw_df=get_ticker_data(TICKER)
 
-#extract features
-df=extract_features_2d(raw_df, lookback=LOOKBACK)
-data_shape=np.shape(df.iloc[0]['data'])
-print(df.iloc[0]['data'])
-print(np.shape(df.iloc[0]['data']))
+    #get features
+    raw_df=create_features(raw_df, lookback=LOOKBACK, label_col=LABEL_COL, lookahead=LOOKAHEAD)
 
-print(df['data'].isna().value_counts())
-print(df['data'].dtypes)
-#data_shape=(10,5,1)
+    #extract features
+    temp=extract_features_cols(raw_df, lookback=LOOKBACK)
+    conv_df=None
 
-#build model
-model=tf.keras.Sequential()
-model.add(tf.keras.Input(shape=data_shape))
-model.add(tf.keras.layers.Conv2D(filters=4, kernel_size=(1,5)))
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(units=(10), activation='relu'))
-model.add(tf.keras.layers.Dense(units=(1), activation='relu'))
+    train_df, test_df = split_test_set(temp)
+    normalize_cols(train_df, set(train_df.columns).difference({'time'}))
+    normalize_cols(test_df, set(train_df.columns).difference({'time'}))
+    y_train=train_df['label']
+    x_train=train_df.drop(['time', 'label', 'price', 'class'], axis=1)
+    x_test=test_df.drop(['time', 'label', 'price', 'class'], axis=1)
+    y_test=test_df['label']
+    print(train_df.columns)
 
-print(model.summary())
+    data_shape=np.shape(x_train)
 
-print(df.iloc[0]['data'])
-print(np.shape(df.iloc[0]['data']))
+    #build dense model
+    dense_model=tf.keras.Sequential()
+    dense_model.add(tf.keras.Input(shape=data_shape))
+    dense_model.add(tf.keras.layers.Dense(units=40, activation='relu'))
+    dense_model.add(tf.keras.layers.Dense(units=(20), activation='relu'))
+    dense_model.add(tf.keras.layers.Dropout(0.3))
+    dense_model.add(tf.keras.layers.Dense(units=(20), activation='relu'))
+    #dense_model.add(tf.keras.layers.Dropout(0.3)) #uncomment for classification
+    dense_model.add(tf.keras.layers.Dense(units=(10), activation='relu'))
+    dense_model.add(tf.keras.layers.Dropout(0.3))
+    dense_model.add(tf.keras.layers.Dense(units=(1), activation='relu'))
+    #dense_model.add(tf.keras.layers.Dense(units=(1), activation='sigmoid')) #uncomment for classification
 
-print(df['data'].isna().value_counts())
-print(df['data'].dtypes)
+    print(dense_model.summary())
 
-#fit model
-model.compile(optimizer='adam', loss='mean_squared_error', metrics='mean_squared_error')
-model.fit(x=df['data'],y=df['label'], validation_split=0.3, shuffle=True, batch_size=1)
+    #fit model
+    dense_model.compile(optimizer='adam', loss='mean_squared_error', metrics='mean_absolute_error')
+    #dense_model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy') #uncomment for classification
+    dense_model.fit(x=x_train, y=y_train, validation_split=0.3, shuffle=True, batch_size=8, epochs=25)
 
-#evaluate model
+    #evaluate model
+    print('Evaluating model')
+    print(np.shape(x_test[str(LOOKBACK)+'_close']))
+    preds=dense_model.predict(x_test)
+    #for i in range(len(preds)):
+    #    print(preds[i], '|', y_test.iloc[i])
+    plot_predictions(preds, y_test, save=False, name='regression'+str(LOOKBACK)+'_'+str(LOOKAHEAD)+'.png')
+    #for t in [0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8]:
+    #    print('Test acc:(', t,')', get_accuracy_diff(preds, y_test, t))
+    #for t in [(0.3, 0.7),(0.4, 0.6),(0.2, 0.8),(0.45, 0.55)]:
+    #    print('Test acc:(', t,')', get_accuracy_diff2(preds, y_test, t[0], t[1]))
+    print(dense_model.evaluate(x_test, y_test, batch_size=1))
+    #print(np.shape(preds), np.shape(y_test))
+    return dense_model.evaluate(x_test, y_test, batch_size=1)
