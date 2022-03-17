d8b8b55f8ac7de3200afb22f5fec47dccb545c52
iagonzal97 <iagonzal97@gmail.com>
2022-03-16 21:31:24 -0500

    some progress on linear regression

--- lin_regression.py
+++ lin_regression.py
@@ -2,11 +2,12 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import seaborn as sns
 
-brazil_data_unclean = pd.read_csv(
-    # "/Users/iangonzalez/Desktop/Spring_2022/CS_573_ML/CS_573_Final_Project/b3_stocks_1994_2020.csv"
-    "b3_stocks_1994_2020.csv"
-)
+brazil_data = pd.read_csv("b3_stocks_1994_2020.csv")
 
-sns.heatmap(
-    brazil_data_unclean.isnull(), yticklabels=False, cbar=False, cmap="cubehelix"
-)
+# print(brazil_data.loc[:, "open"])
+
+ace_data = brazil_data.loc[brazil_data["ticker"] == "ACE 3"]
+
+vale_data = brazil_data.loc[brazil_data["ticker"] == "VALE3"]
+print(vale_data.head())
+print(vale_data.tail())
