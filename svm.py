d0688d4b7476b32e3894881b33fca05beff263c1
iagonzal97 <iagonzal97@gmail.com>
2022-03-17 21:13:23 -0500

    removed lin_regression.ipynb

--- svm.py
+++ svm.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import sklearn.metrics
 import math
+
+brazil_data = pd.read_csv("b3_stocks_1994_2020.csv")
+
+vale_data = brazil_data.loc[brazil_data["ticker"] == "VALE3"]
